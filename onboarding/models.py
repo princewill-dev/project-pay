1b6001a1d8200387db48a9d69112ff66f687570e
princewill-dev <ezekudep@gmail.com>
2024-04-03 22:08:40 +0100

    Add custom fields to User model and implement signup functionality

--- onboarding/models.py
+++ onboarding/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    # your custom fields here
+
+        groups = models.ManyToManyField(
+            Group,
+            verbose_name=_('groups'),
+            blank=True,
+            help_text=_(
+                'The groups this user belongs to. A user will get all permissions '
+                'granted to each of their groups.'
+            ),
+            related_name="custom_user_set",
+            related_query_name="user",
+        )
+
+        user_permissions = models.ManyToManyField(
+            Permission,
+            verbose_name=_('user permissions'),
+            blank=True,
+            help_text=_('Specific permissions for this user.'),
+            related_name="custom_user_set",
+            related_query_name="user",
+        )
+
+
--- onboarding/views.py
+++ onboarding/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login 
+from .forms import SignupForm
 
 def homepage(request):
     return render(request, 'home/index.html')
 
 def signup(request):
     return render(request, 'home/signup.html')
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in directly
+            return redirect('home')  # Redirect to a success page ('home' is an example)
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})

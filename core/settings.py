f7603649b4e9b0c4658223d4025f8a2db7a55f14
princewill-dev <ezekudep@gmail.com>
2024-04-03 21:28:04 +0100

    Delete unused files and images

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
--- onboarding/views.py
+++ onboarding/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'homepage/index.html')
+    return render(request, 'home/index.html')
+
+def signup(request):
+    return render(request, 'home/signup.html')

34dbb14de7b8f21761a63a3966b04c02bffa97e2
princewill-dev <ezekudep@gmail.com>
2024-05-01 15:11:31 +0100

    Update ALLOWED_HOSTS and CORS_ALLOWED_ORIGINS in settings.py, update get_pay_view in views.py, add django-cors-headers to requirements.txt, and delete receive_pay.html template

--- core/settings.py
+++ core/settings.py
@@ -20,13 +20,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "bitwade.fly.dev",
+    "bitwade.onrender.com",
     "127.0.0.1",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1",
-    "https://bitwade.fly.dev",
+    "https://bitwade.onrender.com",
 ]
 
 AUTH_USER_MODEL = 'onboarding.User'
